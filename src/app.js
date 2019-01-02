e57043e574741b9b087fcca0d810a566e89fa78a
QC <cheng305@purdue.edu>
2019-01-01 22:14:00 -0500

    import react-router-dom module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'
+import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my new app.</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from dashboard component.
+    </div>
+);
+const routes = (
+    <BrowserRouter>
+        <Route path='/' component={ExpenseDashboardPage}/>
+    </BrowserRouter>
+);
+    
+
+
+ReactDOM.render(routes, document.getElementById('app'));
