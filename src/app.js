c761a7df8f039b27cce9fe9165573b81cc8430a7
QC <cheng305@purdue.edu>
2019-01-04 17:23:12 -0500

    setting up 404 using switch

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,15 +30,23 @@ const HelpPage = () => (
         This is from HelpPage component.
     </div>
 );
+
+const NotFoundPage = () => (
+    <div>
+        Sorry. 404 Error. Page Not Found.
+    </div>
+);
+
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path='/' component={ExpenseDashboardPage} exact={true}/>
             <Route path='/create' component={AddExpensePage} />
             <Route path='/edit' component={EditExpensePage} />
             <Route path='/help' component={HelpPage} />
-
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
     
 );
