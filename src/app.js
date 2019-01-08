f0c45f7ed04785c59b05df53b1a9221ecaa10f98
QC <cheng305@purdue.edu>
2019-01-08 16:32:27 -0500

    organizing the routes

--- src/app.js
+++ src/app.js
@@ -1,69 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from dashboard component.
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from addexpense component.
-    </div>
-);
-
-
-
-const EditExpensePage = () => (
-    <div>
-        This is from Editexpense component.
-    </div>
-);
-
-
-const HelpPage = () => (
-    <div>
-        This is from HelpPage component.
-    </div>
-);
-
-
-const Header = () => (
-    <div>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Editexpense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </div>
-);
-const NotFoundPage = () => (
-    <div>
-        Sorry. 404 Error. <Link to="/">Go home</Link>
-    </div>
-);
-
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path='/' component={ExpenseDashboardPage} exact={true} />
-                <Route path='/create' component={AddExpensePage} />
-                <Route path='/edit' component={EditExpensePage} />
-                <Route path='/help' component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-
-);
-
-
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+    <div>
+        Sorry. 404 Error. <Link to="/">Go home</Link>
+    </div>
+);
+
+export default NotFoundPage;
