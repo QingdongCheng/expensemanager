94fb4ab659278ab8a374326b57c40321d5e0bd27
QC <cheng305@purdue.edu>
2019-01-09 11:27:06 -0500

    query strings

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from Editexpense component.
-    </div>
-);
+const EditExpensePage = (props) => {
+
+    console.log(props);
+    return (
+        <div>
+            This is from Editexpense component. Id is {props.match.params.id}
+        </div>
+    )
+};
 
 export default EditExpensePage;
